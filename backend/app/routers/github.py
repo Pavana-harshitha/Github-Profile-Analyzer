261ac2f14a8340479e0fe28fe4753c1e93f46bb9
Pavana-Harshitha <pavanaharshitha29@gmail.com>
2026-06-30 10:46:35 +0530

    Added route to get repository details

--- backend/app/routers/github.py
+++ backend/app/routers/github.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 from app.services.github_service import get_profile
+from app.services.github_service import get_profile, get_repositories
+
 router = APIRouter()
 
 
 @router.get("/{username}")
 def get_github_profile(username: str):
     return get_profile(username)
+
+@router.get("/{username}/repos")
+def github_repositories(username: str):
+    return get_repositories(username)
--- backend/app/services/github_service.py
+++ backend/app/services/github_service.py
@@ -1,5 +1,39 @@
 import httpx
 
+def get_repositories(username: str):
+    url = f"https://api.github.com/users/{username}/repos"
+
+    try:
+        response = httpx.get(url, timeout=10)
+
+        if response.status_code == 404:
+            return {"error": "GitHub user not found."}
+
+        response.raise_for_status()
+
+        repos = response.json()
+
+        repository_list = []
+
+        for repo in repos:
+            repository_list.append({
+                "name": repo.get("name"),
+                "language": repo.get("language"),
+                "stars": repo.get("stargazers_count"),
+                "forks": repo.get("forks_count"),
+                "created_at": repo.get("created_at"),
+                "updated_at": repo.get("updated_at"),
+                "html_url": repo.get("html_url"),
+                "description": repo.get("description"),
+            })
+
+        return repository_list
+
+    except httpx.RequestError:
+        return {"error": "Unable to connect to GitHub."}
+
+    except httpx.HTTPStatusError:
+        return {"error": "GitHub returned an unexpected error."}
 
 def get_profile(username: str):
     url = f"https://api.github.com/users/{username}"
